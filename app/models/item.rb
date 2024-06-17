7c3aa66bf3a96886922108c131fec3d55f5b984d
zyunki <iwaju2160@gmail.com>
2024-06-17 21:45:12 +0900

    ActiveHashを導入、バリデーション、アソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,14 @@ class Item < ApplicationRecord
   has_one :order
 
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :product_condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_days
+
+  validates :title, :text, presence: true
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- app/models/shipping_cost.rb
+++ app/models/shipping_cost.rb
@@ -4,4 +4,6 @@ class ShippingCostBearer < ActiveHash::Base
     { id: 2, name: '購入者負担' },
     { id: 3, name: '出品者負担' }
   ]
+  include ActiveHash::Associations
+  has_many :items
 end
