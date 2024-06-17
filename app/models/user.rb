4775d2b19c925ca30517c8655b160268ca899a92
zyunki <iwaju2160@gmail.com>
2024-06-17 20:35:49 +0900

    モデルとテーブルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,6 @@ class User < ApplicationRecord
 
   validates :birthday, presence: true
 
-  #has_many   :items
-  #has_many   :orders
+  has_many   :items
+  has_many   :orders
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_16_065534) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_17_111351) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.integer "price", null: false
+    t.text "product_description", null: false
+    t.integer "product_condition_id", null: false
+    t.integer "category_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "shipping_days_id", null: false
+    t.integer "prefecture_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_16_065534) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
