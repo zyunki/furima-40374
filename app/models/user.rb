8c66345f43d05950a2211ddf9ee2af1ecf198375
zyunki <iwaju2160@gmail.com>
2024-06-17 19:20:54 +0900

    バリデーション設定、テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,23 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :name, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :name, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
+  validates :password, presence: true,
+                       length: { minimum: 6 }, 
+                       format: { with: VALID_PASSWORD_REGEX, message: 'は半角6文字以上と英小文字・数字それぞれ1文字以上含む必要があります' },
+                       confirmation: true
+
+  validates :first_name, :last_name, presence: true,
+                                     format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角で入力してください' }
+
+  validates :first_name_kana, :last_name_kana, presence: true,
+                                               format: { with: /\A[\p{katakana}ー－]+\z/, message: 'は全角カタカナで入力して下さい' }
+
   validates :birthday, presence: true
-  validates :email, uniqueness: true
 
   has_many   :items
   has_many   :orders
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
     name                  { Faker::Name.initials(number: 4) }
-    first_name            { Faker::Name.initials(number: 2) }
-    last_name             { Faker::Name.initials(number: 2) }
-    first_name_kana       { Faker::Name.initials(number: 2) }
-    last_name_kana        { Faker::Name.initials(number: 2) }
+    first_name            { '太郎' } # 全角文字
+    last_name             { '山田' } # 全角文字
+    first_name_kana       { 'タロウ' } # 全角カタカナ
+    last_name_kana        { 'ヤマダ' } # 全角カタカナ
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { 'password1' } # バリデーションを満たすパスワード
     password_confirmation { password }
     birthday              { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
