77f0ad89aa697da881dffcebafb596ad7d42fcec
zyunki <iwaju2160@gmail.com>
2024-06-23 15:06:08 +0900

    商品編集画面遷移、編集、他ユーザーは編集外面へ遷移しない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update,]
+  before_action :contributor_confirmation, only: [:edit, :update,]
 
   def index
     @items = Item.order(created_at: :asc)
@@ -20,7 +22,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報が更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -28,5 +40,14 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name, :price, :product_description, :product_condition_id, :category_id,
                                  :shipping_cost_id, :shipping_day_id, :prefecture_id, :user_id, :price, :image)
+  end
+
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :index, :show]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update]
 end
