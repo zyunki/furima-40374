1e1196126d50ab17fc8909467cff3fe21f4e93db
zyunki <iwaju2160@gmail.com>
2024-07-09 21:11:41 +0900

    プルダウンの内容を修正

--- app/models/shipping_cost.rb
+++ app/models/shipping_cost.rb
@@ -1,8 +1,8 @@
 class ShippingCost < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '購入者負担' },
-    { id: 3, name: '出品者負担' }
+    { id: 2, name: '着払い(購入者負担)' },
+    { id: 3, name: '送料込み(出品者負担)' }
   ]
   include ActiveHash::Associations
   has_many :items
